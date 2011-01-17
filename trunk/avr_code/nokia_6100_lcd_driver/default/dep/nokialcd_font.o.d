7f6e1ee694ac477e8e1c54cdcbd85847b3cd685f
mctasneyr <mctasneyr@43886a23-a647-31fc-ee1d-d2090db33658>
2011-01-17 15:31:07 +0000

    found nokia 6610 driver code for winavr.  Turned into an AvrStudio4 project and trying to port it over to Atmega32u4.  Possibly almost there, but did not take into account the clock speed (16Mhz) vs what the code was written for.

---
+++ trunk/avr_code/nokia_6100_lcd_driver/default/dep/nokialcd_font.o.d
@@ -0,0 +1,6 @@
+nokialcd_font.o: ../nokialcd_font.c ../nokialcd_driver.h \
+  ../nokialcd_font.h
+
+../nokialcd_driver.h:
+
+../nokialcd_font.h:
---
+++ trunk/avr_code/nokia_6100_lcd_driver/default/dep/nokialcd_scroll.o.d
@@ -0,0 +1,10 @@
+nokialcd_scroll.o: ../nokialcd_scroll.c ../nokialcd_controlcodes.h \
+  ../nokialcd_driver.h ../nokialcd_io.h ../nokialcd_scroll.h
+
+../nokialcd_controlcodes.h:
+
+../nokialcd_driver.h:
+
+../nokialcd_io.h:
+
+../nokialcd_scroll.h:
